ca72ad63455a3d0eb8775505c2f2c72e0fda477e
bhaskarx6 <bhaskar.neogi@nzminds.com>
2025-09-15 14:52:42 +0530

    fix: tsconfig fix, ignore buildinfo

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PublicClientApplication } from '@azure/msal-browser';
 
 import './index.css'; //! I will remove it probably - not needed
-import App from './App.tsx';
+import App from './App';
 import '@styles/variables.css';
 import '@styles/global.css';
-import { AuthProvider } from '@hooks/use-auth.tsx';
+import { AuthProvider } from '@hooks/use-auth';
 import { msalConfig } from './config/auth-config';
 import './lib/i18n.ts';
 
